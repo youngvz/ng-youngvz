f09b6519c064d2b092f793960c85e44e0b3bd211
youngvz <shah.a.viraj@gmail.com>
2021-02-09 15:00:06 -0500

    Updated Home Component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,11 +5,31 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
+
+
 export class HomeComponent implements OnInit {
+  audio = new Audio();
 
   constructor() { }
 
   ngOnInit(): void {
+    this.audio.src = "../../assets/audio/HOME - Resonance.mp3";
+    this.audio.load();
+
+    this.audio.addEventListener('ended', function() {
+      this.currentTime = 0;
+      this.play();
+    }, false);
   }
 
+
+  playAudio(){
+
+    if (this.audio.duration > 0 && !this.audio.paused){
+      this.audio.pause();
+    } else {
+      this.audio.play();
+    }
+
+  }
 }
