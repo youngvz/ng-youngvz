2993b196cbed6b6cf8d25fce7bdc1ff3c2e42b8a
youngvz <shah.a.viraj@gmail.com>
2021-02-09 02:01:45 -0500

    Updated Home Component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,8 @@ import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full'},
-  { path: 'home', component: HomeComponent }
+  { path: 'home', component: HomeComponent },
+  { path: '**', redirectTo: 'home' }
 ];
 
 @NgModule({
